7326edf19e52a380def0ab296db5f9af51222ce9
sohaila-elbasel <sohailaelbasel@hotmail.com>
2020-09-05 18:57:50 +0200

    add comments

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,7 @@
 from blog import app, db
 from blog.models import Article, Comment
 from blog.forms import AddArticle, AddComment
-from flask import render_template
+from flask import render_template, redirect, url_for
 
 @app.route('/')
 def home():
@@ -9,8 +9,19 @@ def home():
     return render_template('home.html', articles = articles)
 
 
-@app.route('/article/<int:article_id>')
+@app.route('/article/<int:article_id>', methods=['POST', 'GET'])
 def article(article_id):
     form = AddComment()
     article = Article.query.get(article_id)
-    return render_template('article.html', article = article, form = form)
+
+    #display comments
+    comments = Comment.query.all()
+
+    #Add comment
+    if form.validate_on_submit():
+        comment = Comment(title=form.comment_title.data, content = form.comment_content.data, article_id = article_id)
+        db.session.add(comment)
+        db.session.commit()
+        return redirect(url_for('article', article_id = article_id))
+
+    return render_template('article.html', article = article, form = form, comments = comments)
